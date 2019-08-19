e75b6da52b7700a9641ce799cac6ad9b4e2153cc
Patrick Collins <PatrickCP0@Gmail.com>
2019-08-19 14:57:09 -0700

    to back to flattening; can't change expected type.
    not sure why this worked before; interface must have changed?

--- workers.tf
+++ workers.tf
@@ -139,7 +139,7 @@ resource "aws_launch_configuration" "workers" {
     "public_ip",
     local.workers_group_defaults["public_ip"],
   )
-  security_groups = [local.worker_security_group_id, var.worker_additional_security_group_ids, compact(
+  security_groups = flatten([local.worker_security_group_id, var.worker_additional_security_group_ids, compact(
     split(
       ",",
       lookup(
@@ -148,7 +148,7 @@ resource "aws_launch_configuration" "workers" {
         local.workers_group_defaults["additional_security_group_ids"],
       ),
     ),
-  )]
+  )])
   iam_instance_profile = aws_iam_instance_profile.workers.*.id[count.index]
   image_id = lookup(
     var.worker_groups[count.index],
